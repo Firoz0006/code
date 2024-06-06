38ebdf1c1d989d5ec77db059977e7f3599558a1d
Md Firoz Alam <99121808+Firoz0006@users.noreply.github.com>
2024-06-06 17:03:03 +0530

    Add files via upload

---
+++ Basic1/demo.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"hello\n";
+    //cout<<"hello";
+    return 0;
+}
+
---
+++ Basic1/s1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace  std;
+    int main(){
+        double dbl=5.6;
+        int res=(int)dbl+10;
+        cout<<res<<endl;
+        return 0;
+    }
+    
+
